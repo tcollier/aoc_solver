28d5e344f16a1cbf26d392fe2af279fd7dd50a8d
Tom Collier <collier@apartmentlist.com>
2020-12-12 14:30:29 -0800

    Print value of accumulator before first repeated instruction

---
+++ 2020/day8.py
@@ -0,0 +1,24 @@
+INPUT = open("day8_input.txt", "r").readlines()
+
+
+def print_part1_ans(input):
+    instructions = [l.rstrip() for l in input]
+    acc = 0
+    curr_ptr = 0
+    visited = set()
+    while curr_ptr < len(instructions):
+        if curr_ptr in visited:
+            print(acc)
+            break
+        visited.add(curr_ptr)
+        instruction = instructions[curr_ptr]
+        if instruction[0:3] == "acc":
+            acc = acc + int(instruction[4:])
+            curr_ptr = curr_ptr + 1
+        elif instruction[0:3] == "jmp":
+            curr_ptr = curr_ptr + int(instruction[4:])
+        elif instruction[0:3] == "nop":
+            curr_ptr = curr_ptr + 1
+
+
+print_part1_ans(INPUT)
