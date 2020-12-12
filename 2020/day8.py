f71eb0d1a08e95de677919090c43223e1aa80fb5
Tom Collier <collier@apartmentlist.com>
2020-12-12 14:38:16 -0800

    Fix program and print accumulator at termination

--- 2020/day8.py
+++ 2020/day8.py
@@ -21,4 +21,41 @@ def print_part1_ans(input):
             curr_ptr = curr_ptr + 1
 
 
-print_part1_ans(INPUT)
+def run(instructions):
+    acc = 0
+    curr_ptr = 0
+    visited = set()
+    while curr_ptr < len(instructions):
+        if curr_ptr in visited:
+            raise Exception("Infinite loop detected")
+        visited.add(curr_ptr)
+        instruction = instructions[curr_ptr]
+        if instruction[0:3] == "acc":
+            acc = acc + int(instruction[4:])
+            curr_ptr = curr_ptr + 1
+        elif instruction[0:3] == "jmp":
+            curr_ptr = curr_ptr + int(instruction[4:])
+        elif instruction[0:3] == "nop":
+            curr_ptr = curr_ptr + 1
+    return acc
+
+
+def print_part2_ans(input):
+    instructions = [l.rstrip() for l in input]
+    for i in range(len(instructions)):
+        orig = instructions[i]
+        if orig[0:3] == "jmp":
+            instructions[i] = f"nop {orig[4:]}"
+        elif orig[0:3] == "nop":
+            instructions[i] = f"jmp {orig[4:]}"
+        else:
+            continue
+
+        try:
+            print(run(instructions))
+        except:
+            pass
+        instructions[i] = orig
+
+
+print_part2_ans(INPUT)
