acde0110b710854d82eed1ebcfabccbc105f40d4
Tom Collier <collier@apartmentlist.com>
2020-12-12 13:36:35 -0800

    Print sum of sum of group positive responses pt 2

--- 2020/day6.py
+++ 2020/day6.py
@@ -15,4 +15,22 @@ def print_part1_ans(input):
     print(total_answered + len(group_answers))
 
 
-print_part1_ans(INPUT)
+def print_part2_ans(input):
+    total_answered = 0
+    first_person = True
+    for line in input:
+        if line == "\n":
+            total_answered = total_answered + len(group_answers)
+            first_person = True
+            continue
+        person_answers = {q for q in line.rstrip()}
+        if first_person:
+            group_answers = person_answers
+            first_person = False
+        else:
+            group_answers = group_answers.intersection(person_answers)
+
+    print(total_answered + len(group_answers))
+
+
+print_part2_ans(INPUT)
