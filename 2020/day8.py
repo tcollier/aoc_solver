8d3f4761a944bdcb7117a40b62420aaf6f63e11f
Tom Collier <collier@apartmentlist.com>
2020-12-12 18:53:57 -0800

    Refactor part 1 to use run function

--- 2020/day8.py
+++ 2020/day8.py
@@ -1,33 +1,16 @@
 INPUT = open("day8_input.txt", "r").readlines()
 
 
-def print_part1_ans(input):
-    instructions = [l.rstrip() for l in input]
+def run(instructions, fail_on_loop):
     acc = 0
     curr_ptr = 0
     visited = set()
     while curr_ptr < len(instructions):
         if curr_ptr in visited:
-            print(acc)
-            break
-        visited.add(curr_ptr)
-        instruction = instructions[curr_ptr]
-        if instruction[0:3] == "acc":
-            acc = acc + int(instruction[4:])
-            curr_ptr = curr_ptr + 1
-        elif instruction[0:3] == "jmp":
-            curr_ptr = curr_ptr + int(instruction[4:])
-        elif instruction[0:3] == "nop":
-            curr_ptr = curr_ptr + 1
-
-
-def run(instructions):
-    acc = 0
-    curr_ptr = 0
-    visited = set()
-    while curr_ptr < len(instructions):
-        if curr_ptr in visited:
-            raise Exception("Infinite loop detected")
+            if fail_on_loop:
+                raise Exception("Infinite loop detected")
+            else:
+                return acc
         visited.add(curr_ptr)
         instruction = instructions[curr_ptr]
         if instruction[0:3] == "acc":
@@ -38,6 +21,11 @@ def run(instructions):
         elif instruction[0:3] == "nop":
             curr_ptr = curr_ptr + 1
     return acc
+
+
+def print_part1_ans(input):
+    instructions = [l.rstrip() for l in input]
+    print(run(instructions, False))
 
 
 def print_part2_ans(input):
@@ -52,7 +40,7 @@ def print_part2_ans(input):
             continue
 
         try:
-            print(run(instructions))
+            print(run(instructions, True))
             break
         except:
             pass
