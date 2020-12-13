f3eb550251da21a42e9e16d7473a2ebd4e2aeea1
Tom Collier <collier@apartmentlist.com>
2020-12-12 23:00:00 -0800

    Use math built-in for lcm

--- 2020/day13.py
+++ 2020/day13.py
@@ -16,40 +16,8 @@ def print_part1_ans(input):
     print(min_wait_id * min_wait)
 
 
-def merge_factors(f1, f2):
-    factors = {}
-    for fact in set([k for k in f1.keys()] + [k for k in f2.keys()]):
-        count1 = f1.get(fact, 0)
-        count2 = f2.get(fact, 0)
-        factors[fact] = max(count1, count2)
-    return factors
-
-
-def factor(n):
-    factors = {}
-    while n > 1:
-        for i in range(2, n + 1):
-            if n % i == 0:
-                if i not in factors:
-                    factors[i] = 0
-                factors[i] = factors[i] + 1
-                n = n // i
-                break
-    return factors
-
-
-def product(f):
-    p = 1
-    for fact, exp in f.items():
-        p = p * (fact ** exp)
-    return p
-
-
-def lcm(vals):
-    factors = {}
-    for val in vals:
-        factors = merge_factors(factors, factor(val))
-    return product(factors)
+def lcm(a, b):
+    return abs(a * b) // math.gcd(a, b)
 
 
 def print_part2_ans(input):
@@ -61,7 +29,7 @@ def print_part2_ans(input):
     for offset, bus_id in bus_ids.items():
         while (timestamp + offset) % bus_id != 0:
             timestamp = timestamp + incrementer
-        incrementer = lcm([incrementer, bus_id])
+        incrementer = lcm(incrementer, bus_id)
     print(timestamp)
 
 
