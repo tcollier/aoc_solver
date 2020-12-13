e17ad5b2d945172cf908bf4d398cfb326b575fbc
Tom Collier <collier@apartmentlist.com>
2020-12-12 18:28:28 -0800

    Only look at numbers to right of first index

--- 2020/day1.py
+++ 2020/day1.py
@@ -1,5 +1,6 @@
 INPUT = [int(l) for l in open("day1_input.txt", "r").readlines()]
 
+
 def pair_with_sum(arr, sum):
     vals = set()
     for val in arr:
@@ -13,14 +14,10 @@ def triple_with_sum(arr, sum):
     arr.sort()
     for val_index, val in enumerate(arr):
         diff = sum - val
-        i = 0
+        i = val_index + 1
         j = len(arr) - 1
         while i < j:
-            if i == val_index:
-                i = i + 1
-            elif j == val_index:
-                j = j - 1
-            elif arr[i] + arr[j] + val == sum:
+            if arr[i] + arr[j] + val == sum:
                 return (val, arr[i], arr[j])
             elif arr[i] + arr[j] + val < sum:
                 i = i + 1
@@ -30,14 +27,12 @@ def triple_with_sum(arr, sum):
 
 def print_part1_ans():
     pair = pair_with_sum(INPUT, 2020)
-    print("Pair:", pair)
-    print("Product:", pair[0] * pair[1])
+    print(pair[0] * pair[1])
 
 
 def print_part2_ans():
     trip = triple_with_sum(INPUT, 2020)
-    print("Triplet:", trip)
-    print("Product:", trip[0] * trip[1] * trip[2])
+    print(trip[0] * trip[1] * trip[2])
 
 
 print_part2_ans()
