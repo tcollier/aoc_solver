0c4a2a2dd04a073f2de5db57afc2ea38156840ef
Tom Collier <collier@apartmentlist.com>
2020-12-12 18:41:15 -0800

    Use more pythonic way to check if value is in range

--- 2020/day4.py
+++ 2020/day4.py
@@ -6,22 +6,24 @@ REQUIRED_FIELDS = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
 
 
 def year_between(val, min, max):
-    if re.match("^\d{4}$", val):
+    if re.match(r"^\d{4}$", val):
         int_val = int(val)
-        return int_val >= min and int_val <= max
+        return min <= int_val <= max
     else:
         return False
 
+
 def valid_height(val):
-    match = re.match("^(\d+)(in|cm)$", val)
-    if match and match[2] == 'cm':
+    match = re.match(r"^(\d+)(in|cm)$", val)
+    if match and match[2] == "cm":
         int_val = int(match[1])
-        return int_val >= 150 and int_val <= 193
-    elif match and match[2] == 'in':
+        return 150 <= int_val <= 193
+    elif match and match[2] == "in":
         int_val = int(match[1])
-        return int_val >= 59 and int_val <= 76
+        return 59 <= int_val <= 76
     else:
         return False
+
 
 VALID_EYE_COLORS = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
 PART2_VALIDATION = {
@@ -29,10 +31,11 @@ PART2_VALIDATION = {
     "iyr": lambda val: year_between(val, 2010, 2020),
     "eyr": lambda val: year_between(val, 2020, 2030),
     "hgt": valid_height,
-    "hcl": lambda val: re.match("^#[\da-f]{6}$", val) is not None,
+    "hcl": lambda val: re.match(r"^#[\da-f]{6}$", val) is not None,
     "ecl": lambda val: val in VALID_EYE_COLORS,
-    "pid": lambda val: re.match("^\d{9}$", val) is not None
+    "pid": lambda val: re.match(r"^\d{9}$", val) is not None,
 }
+
 
 def part1_validation(field, val):
     return True
@@ -43,7 +46,6 @@ def part2_validation(field, val):
 
 
 def print_ans(input, validation_fn):
-    valid = False
     needed_fields = {f for f in REQUIRED_FIELDS}
     num_valid = 0
     for line in input:
@@ -52,7 +54,6 @@ def print_ans(input, validation_fn):
                 num_valid = num_valid + 1
             needed_fields = {f for f in REQUIRED_FIELDS}
             continue
-
         parts = line.rstrip().split(" ")
         for part in parts:
             field, val = part.split(":")
@@ -63,4 +64,4 @@ def print_ans(input, validation_fn):
     print(num_valid)
 
 
-print_ans(INPUT, part2_validation)
+print_ans(INPUT, part_validation)
