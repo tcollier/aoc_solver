3a98d6532f6ce587042e650bf9e6888ad3343a54
Tom Collier <collier@apartmentlist.com>
2020-12-12 17:03:34 -0800

    Refactor to allow different rules during iteration

--- 2020/day11.py
+++ 2020/day11.py
@@ -1,19 +1,24 @@
 INPUT = open("day11_input.txt", "r").readlines()
 
 
-def iterate(seats):
+def part1_occupied_count(seats, row, col):
+    num_occupied = 0
+    for adj_row in range(max(0, row - 1), min(row + 2, len(seats))):
+        for adj_col in range(max(0, col - 1), min(col + 2, len(seats[adj_row]))):
+            if row == adj_row and col == adj_col:
+                continue
+            if seats[adj_row][adj_col] == "#":
+                num_occupied = num_occupied + 1
+    return num_occupied
+
+
+def iterate(seats, occupiend_count_fn):
     next_seats = [[s for s in row] for row in seats]
     for row in range(len(seats)):
         for col in range(len(seats[row])):
             if seats[row][col] == ".":
                 continue
-            num_occupied = 0
-            for adj_row in range(max(0, row - 1), min(row + 2, len(seats))):
-                for adj_col in range(max(0, col - 1), min(col + 2, len(seats[adj_row]))):
-                    if row == adj_row and col == adj_col:
-                        continue
-                    if seats[adj_row][adj_col] == "#":
-                        num_occupied = num_occupied + 1
+            num_occupied = occupiend_count_fn(seats, row, col)
             if seats[row][col] == "L" and num_occupied == 0:
                 next_seats[row][col] = "#"
             elif seats[row][col] == "#" and num_occupied >= 4:
@@ -30,12 +35,12 @@ def count_occupied(seats):
     return total
 
 
-def print_part1_ans(input):
+def print_ans(input, occupiend_count_fn):
     seats = [[s for s in l.rstrip()] for l in input]
     iterations = 0
     prev_seats = None
     while True:
-        seats = iterate(seats)
+        seats = iterate(seats, occupiend_count_fn)
         if seats == prev_seats:
             break
         iterations = iterations + 1
@@ -43,4 +48,4 @@ def print_part1_ans(input):
     print(count_occupied(seats))
 
 
-print_part1_ans(INPUT)
+print_ans(INPUT, part1_occupied_count)
