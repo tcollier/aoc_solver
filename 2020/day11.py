5acd2dc3b7d750dcef29d4c3cb83aaa6edf9575f
Tom Collier <collier@apartmentlist.com>
2020-12-12 19:00:35 -0800

    Extract chars to constants

--- 2020/day11.py
+++ 2020/day11.py
@@ -1,5 +1,20 @@
 INPUT = open("day11_input.txt", "r").readlines()
 
+DIRECTIONS = [
+    (-1, -1),
+    (-1, 0),
+    (-1, 1),
+    (0, -1),
+    (0, 1),
+    (1, -1),
+    (1, 0),
+    (1, 1),
+]
+
+FLOOR = "."
+EMPTY = "L"
+OCCUPIED = "#"
+
 
 def part1_occupied_count(seats, row, col):
     num_occupied = 0
@@ -7,27 +22,26 @@ def part1_occupied_count(seats, row, col):
         for adj_col in range(max(0, col - 1), min(col + 2, len(seats[adj_row]))):
             if row == adj_row and col == adj_col:
                 continue
-            if seats[adj_row][adj_col] == "#":
+            if seats[adj_row][adj_col] == OCCUPIED:
                 num_occupied = num_occupied + 1
     return num_occupied
 
-
-DIRECTIONS = [
-    (-1, -1), (-1, 0), (-1, 1),
-    ( 0, -1),          ( 0, 1),
-    ( 1, -1), ( 1, 0), ( 1, 1),
-]
 
 def part2_occupied_count(seats, row, col):
     num_occupied = 0
     for direction in DIRECTIONS:
         adj_row = row + direction[0]
         adj_col = col + direction[1]
-        while adj_row >= 0 and adj_row < len(seats) and adj_col >= 0 and adj_col < len(seats[adj_row]):
-            if seats[adj_row][adj_col] == "#":
+        while (
+            adj_row >= 0
+            and adj_row < len(seats)
+            and adj_col >= 0
+            and adj_col < len(seats[adj_row])
+        ):
+            if seats[adj_row][adj_col] == OCCUPIED:
                 num_occupied = num_occupied + 1
                 break
-            if seats[adj_row][adj_col] == "L":
+            if seats[adj_row][adj_col] == EMPTY:
                 break
             adj_row = adj_row + direction[0]
             adj_col = adj_col + direction[1]
@@ -39,13 +53,13 @@ def iterate(seats, visible_threshold, occupiend_count_fn):
     next_seats = [[s for s in row] for row in seats]
     for row in range(len(seats)):
         for col in range(len(seats[row])):
-            if seats[row][col] == ".":
+            if seats[row][col] == FLOOR:
                 continue
             num_occupied = occupiend_count_fn(seats, row, col)
-            if seats[row][col] == "L" and num_occupied == 0:
-                next_seats[row][col] = "#"
-            elif seats[row][col] == "#" and num_occupied >= visible_threshold:
-                next_seats[row][col] = "L"
+            if seats[row][col] == EMPTY and num_occupied == 0:
+                next_seats[row][col] = OCCUPIED
+            elif seats[row][col] == OCCUPIED and num_occupied >= visible_threshold:
+                next_seats[row][col] = EMPTY
     return next_seats
 
 
@@ -53,7 +67,7 @@ def count_occupied(seats):
     total = 0
     for row in range(len(seats)):
         for col in range(len(seats[row])):
-            if seats[row][col] == "#":
+            if seats[row][col] == OCCUPIED:
                 total += 1
     return total
 
