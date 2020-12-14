177b35341003a08c0fbc6052d4a176f2f3f372fa
Tom Collier <collier@apartmentlist.com>
2020-12-13 22:36:28 -0800

    Code clean up

--- 2020/day14.py
+++ 2020/day14.py
@@ -8,8 +8,8 @@ BIT_LEN = 36
 def parse_mask_part1(raw):
     zeros_mask = ones_mask = 0
     for i in range(BIT_LEN):
-        zeros_mask <<= 1  # Mask where zeros have an effect (NAND)
-        ones_mask <<= 1  # Mask where ones have an effect (OR)
+        zeros_mask <<= 1  # Mask where zeros have an effect
+        ones_mask <<= 1  # Mask where ones have an effect
         if raw[i] != "0":
             zeros_mask += 1
         if raw[i] == "1":
@@ -18,7 +18,6 @@ def parse_mask_part1(raw):
 
 
 def print_part1_ans(input):
-    zeros_mask = ones_mask = None
     values = {}
     for line in input:
         match = re.match(r"^(.+) = (.+)$", line)
@@ -27,10 +26,7 @@ def print_part1_ans(input):
         else:
             addr_match = re.match(r"^mem\[(\d+)\]$", match[1])
             addr = addr_match[1]
-            raw_value = int(match[2])
-            value = raw_value & zeros_mask
-            value |= ones_mask
-            values[addr] = value
+            values[addr] = int(match[2]) & zeros_mask | ones_mask
     total = 0
     for value in values.values():
         total += value
@@ -39,38 +35,38 @@ def print_part1_ans(input):
 
 def parse_mask_part2(raw):
     ones_mask = 0
-    floating_bits = []
+    floating_masks = []
+    floating_mask = 1 << BIT_LEN
     for i in range(BIT_LEN):
-        ones_mask <<= 1  # Mask where ones have an effect (OR)
+        floating_mask >>= 1
+        ones_mask <<= 1  # Mask where ones have an effect
         if raw[i] == "1":
             ones_mask += 1
         elif raw[i] == "X":
-            floating_bits.append(BIT_LEN - i - 1)
-    return ones_mask, floating_bits
+            floating_masks.append(floating_mask)
+    return ones_mask, floating_masks
 
 
-def store_part_2(values, addr, floating_bits, value, index=0):
-    if index == len(floating_bits):
+def store_part_2(values, addr, floating_masks, value, index=0):
+    if index == len(floating_masks):
         values[addr] = value
         return
-    mask = 1 << floating_bits[index]
-    store_part_2(values, addr | mask, floating_bits, value, index + 1)
-    store_part_2(values, addr & ~mask, floating_bits, value, index + 1)
+    addr_with_0 = addr & ~floating_masks[index]
+    addr_with_1 = addr | floating_masks[index]
+    store_part_2(values, addr_with_0, floating_masks, value, index + 1)
+    store_part_2(values, addr_with_1, floating_masks, value, index + 1)
 
 
 def print_part2_ans(input):
-    ones_mask = None
-    floating_bits = []
     values = {}
     for line in input:
         match = re.match(r"^(.+) = (.+)$", line)
         if match[1] == "mask":
-            ones_mask, floating_bits = parse_mask_part2(match[2])
+            ones_mask, floating_masks = parse_mask_part2(match[2])
         else:
             addr_match = re.match(r"^mem\[(\d+)\]$", match[1])
-            raw_addr = int(addr_match[1])
-            addr = raw_addr | ones_mask
-            store_part_2(values, addr, floating_bits, int(match[2]))
+            base_addr = int(addr_match[1]) | ones_mask
+            store_part_2(values, base_addr, floating_masks, int(match[2]))
     total = 0
     for value in values.values():
         total += value
