fdd9240723039e9c77b0b6a84fff7d3621d65863
Tom Collier <collier@apartmentlist.com>
2020-12-14 21:49:19 -0800

    Slight optimizations

--- 2020/day15.py
+++ 2020/day15.py
@@ -1,28 +1,20 @@
 INPUT = [14, 3, 1, 0, 9, 5]
 
 
-def write_val(val, i, prev_last_usage, last_usage):
-    if val in last_usage:
-        prev_last_usage[val] = last_usage[val]
-    last_usage[val] = i + 1
-
-
 def print_ans(input, n):
-    curr_num = None
     prev_last_usage = {}
     last_usage = {}
-    for i in range(n):
-        if i < len(input):
-            curr_num = input[i]
-            write_val(curr_num, i, prev_last_usage, last_usage)
+    for i, curr_num in enumerate(input):
+        last_usage[curr_num] = i
+    for i in range(len(input), n):
+        if curr_num not in prev_last_usage:
+            curr_num = 0
         else:
-            last_val = curr_num
-            if last_val not in prev_last_usage:
-                curr_num = 0
-            else:
-                curr_num = last_usage[curr_num] - prev_last_usage[curr_num]
-            write_val(curr_num, i, prev_last_usage, last_usage)
-        print(curr_num)
+            curr_num = last_usage[curr_num] - prev_last_usage[curr_num]
+        if curr_num in last_usage:
+            prev_last_usage[curr_num] = last_usage[curr_num]
+        last_usage[curr_num] = i
+    print(curr_num)
 
 
 print_ans(INPUT, 30000000)
