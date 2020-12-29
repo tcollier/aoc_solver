99599d582b41451e827f82dea1561def28779cd5
Tom Collier <collier@apartmentlist.com>
2020-12-28 21:27:05 -0800

    Use complex numbers for 20% performance improvement

--- 2020/day11.py
+++ 2020/day11.py
@@ -1,93 +1,76 @@
 INPUT = [l.rstrip() for l in open("day11_input.txt", "r").readlines()]
 
-DIRECTIONS = [
-    (-1, -1),
-    (-1, 0),
-    (-1, 1),
-    (0, -1),
-    (0, 1),
-    (1, -1),
-    (1, 0),
-    (1, 1),
-]
+DIRECTIONS = [-1 - 1j, -1j, 1 - 1j, -1, 1, -1 + 1j, 1j, 1 + 1j]
 
 FLOOR = "."
 EMPTY = "L"
 OCCUPIED = "#"
 
 
-def part1_occupied_count(seats, row, col):
-    num_occupied = 0
-    for adj_row in range(max(0, row - 1), min(row + 2, len(seats))):
-        for adj_col in range(max(0, col - 1), min(col + 2, len(seats[adj_row]))):
-            if row == adj_row and col == adj_col:
-                continue
-            if seats[adj_row][adj_col] == OCCUPIED:
-                num_occupied += 1
-    return num_occupied
-
-
-def part2_occupied_count(seats, row, col):
+def part1_occupied_count(occupied, floor, seat, max_seat):
     num_occupied = 0
     for direction in DIRECTIONS:
-        adj_row = row + direction[0]
-        adj_col = col + direction[1]
-        while (
-            adj_row >= 0
-            and adj_row < len(seats)
-            and adj_col >= 0
-            and adj_col < len(seats[adj_row])
-        ):
-            if seats[adj_row][adj_col] == OCCUPIED:
+        neighbor = seat + direction
+        if 0 <= neighbor.real < max_seat.real and 0 <= neighbor.imag < max_seat.imag:
+            if neighbor in occupied:
                 num_occupied += 1
-                break
-            if seats[adj_row][adj_col] == EMPTY:
-                break
-            adj_row += direction[0]
-            adj_col += direction[1]
-
     return num_occupied
 
 
-def iterate(seats, visible_threshold, occupiend_count_fn):
-    next_seats = [[s for s in row] for row in seats]
-    for row in range(len(seats)):
-        for col in range(len(seats[row])):
-            if seats[row][col] == FLOOR:
+def part2_occupied_count(occupied, floor, seat, max_seat):
+    num_occupied = 0
+    for direction in DIRECTIONS:
+        neighbor = seat + direction
+        while 0 <= neighbor.real < max_seat.real and 0 <= neighbor.imag < max_seat.imag:
+            if neighbor in occupied:
+                num_occupied += 1
+                break
+            elif neighbor not in floor:
+                break
+            neighbor += direction
+    return num_occupied
+
+
+def iterate(occupied, floor, max_seat, visible_threshold, occupiend_count_fn):
+    next_occupied = set()
+    for row in range(round(max_seat.imag)):
+        for col in range(round(max_seat.real)):
+            seat = col + row * 1j
+            if seat in floor:
                 continue
-            num_occupied = occupiend_count_fn(seats, row, col)
-            if seats[row][col] == EMPTY and num_occupied == 0:
-                next_seats[row][col] = OCCUPIED
-            elif seats[row][col] == OCCUPIED and num_occupied >= visible_threshold:
-                next_seats[row][col] = EMPTY
-    return next_seats
-
-
-def count_occupied(seats):
-    total = 0
-    for row in range(len(seats)):
-        for col in range(len(seats[row])):
-            if seats[row][col] == OCCUPIED:
-                total += 1
-    return total
-
-
-def print_seats(seats):
-    print("\n".join(["".join(r) for r in seats]))
+            num_occupied = occupiend_count_fn(occupied, floor, col + row * 1j, max_seat)
+            if seat not in occupied and num_occupied == 0:
+                next_occupied.add(seat)
+            elif seat in occupied and num_occupied < visible_threshold:
+                next_occupied.add(seat)
+    return next_occupied
 
 
 def print_ans(input, visible_threshold, occupied_count_fn):
-    seats = [[s for s in l] for l in input]
-    iterations = 0
-    prev_seats = None
+    occupied = set()
+    floor = set()
+    for row, line in enumerate(input):
+        for col, c in enumerate(line):
+            if c == OCCUPIED:
+                occupied.add(col + row * 1j)
+            elif c == FLOOR:
+                floor.add(col + row * 1j)
+
+    prev_occupied = None
     while True:
-        seats = iterate(seats, visible_threshold, occupied_count_fn)
-        if seats == prev_seats:
+        occupied = iterate(
+            occupied,
+            floor,
+            len(input[0]) + len(input) * 1j,
+            visible_threshold,
+            occupied_count_fn,
+        )
+        if occupied == prev_occupied:
             break
-        iterations += 1
-        prev_seats = seats
+        prev_occupied = occupied
 
-    print(count_occupied(seats))
+    print(len(occupied))
 
 
+print_ans(INPUT, 4, part1_occupied_count)
 print_ans(INPUT, 5, part2_occupied_count)
