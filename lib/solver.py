10ac40dc2ad8743a7fdc11dfc04edb15b3068c1a
Tom Collier <collier@apartmentlist.com>
2021-01-04 10:20:43 -0800

    Refactor engine/UI logic
    Have the solver engine fire events into a pipe and have the UI
    react to those events.

--- lib/solver.py
+++ lib/solver.py
@@ -1,186 +1,156 @@
 import json
 import os
-import time
 import traceback
 
 from datetime import datetime
 
-from lib.concurrency import concurrent_with_spinner, fn_with_spinner
-from lib.format import (
-    Color,
-    format_attempt,
-    format_building,
-    format_diff,
-    format_failure,
-    format_running,
-    format_success,
-    format_timing,
-)
 from lib.languages import language_config, all_languages
 from lib.shell import ShellException, shell_out
+from lib.solver_event import SolverEvent
 
 
-def _find_files(language, year, day):
-    languages = language if language else all_languages()
-    for l in languages:
-        if l not in all_languages():
-            raise Exception(
-                f"Unrecognized language: {l} (available: {', '.join(all_languages())})"
-            )
-    if not os.path.isdir(year):
-        raise Exception(f"Invalid year {year}")
-    padded_day = day.zfill(2)
-    if not os.path.isdir(os.path.join(year, padded_day)):
-        raise Exception(f"Invalid day {day} for {year}")
-    found = False
-    for l in languages:
-        filename = os.path.join(
-            year, padded_day, f"main.{language_config(l).extension}"
-        )
-        if os.path.isfile(filename):
-            found = True
-            yield l, filename
+class LanguageSolver(object):
+    def __init__(self, conn, language, year, day, filename):
+        self.conn = conn
+        self.language = language
+        self.year = year
+        self.day = day
+        self.filename = filename
+        self.config = language_config(language)
 
+    def __call__(self, expected, outfile):
+        cmd = self._build()
+        actual = self._solve(cmd)
+        if not expected:
+            self._handle_output(actual, outfile)
+        elif actual == expected:
+            self._handle_success(cmd)
+        else:
+            self._handle_invalid_output(expected, actual)
 
-def _output_file(year, day):
-    return os.path.join(year, day.zfill(2), "output.txt")
+    def _dispatch(self, event, args={}):
+        args["language"] = self.language
+        args["year"] = self.year
+        args["day"] = self.day
+        self.conn.send(event)
+        self.conn.send(args)
 
+    def _build(self):
+        def curried_build():
+            return self.config.cmd_fn(self.config.build_fn(self.filename))
 
-def _find_solution(filename):
-    if os.path.isfile(filename):
-        return "".join(open(filename, "r").readlines())
-    else:
-        return None
+        if not self.config.has_build_step():
+            return curried_build()
 
-
-def _time_with_spinner(cmd, pipe):
-    def fn():
-        start_time = datetime.now()
-        timing_info = json.loads(shell_out(f"{cmd} --time"))
-        duration = datetime.now() - start_time
-        return format_timing(timing_info, duration)
-
-    print(f"{Color.GREY}timing{Color.ENDC} ", end="", flush=True)
-    fn_with_spinner(fn, pipe)
-
-
-def _solve_with_spinner(cmd, pipe):
-    def fn():
-        return shell_out(cmd)
-
-    fn_with_spinner(fn, pipe)
-
-
-def _build_with_spinner(config, filename, pipe):
-    def fn():
-        return config.cmd_fn(config.build_fn(filename))
-
-    fn_with_spinner(fn, pipe)
-
-
-def _build(config, filename, language, year, day):
-    if not config.has_build_step():
-        return config.cmd_fn(config.build_fn(filename))
-
-    print(format_building(language, year, day), end=" ", flush=True)
-    try:
-        cmd = concurrent_with_spinner(_build_with_spinner, config, filename)
-        print("\033[A")
-        return cmd
-    except ShellException as e:
-        print("\033[A")
-        print(format_failure(language, year, day), end="  \n")
-        print(e.stderr, end="")
-        raise e
-    except Exception as e:
-        print("\033[A")
-        print(format_failure(language, year, day), end="  \n")
-        traceback.print_exc()
-        raise e
-
-
-def _run(cmd, language, year, day):
-    print(format_running(language, year, day), end=" ", flush=True)
-    try:
-        actual = concurrent_with_spinner(_solve_with_spinner, cmd)
-        print("\033[A")  # Move cursor back to start of line
-        return actual
-    except ShellException as e:
-        print("\033[A")
-        print(format_failure(language, year, day), end="  \n")
-        print(e.stderr)
-        raise e
-    except Exception as e:
-        print("\033[A")
-        print(format_failure(language, year, day), end="  \n")
-        traceback.print_exc()
-        raise e
-
-
-def _handle_attempt(language, year, day, actual, save):
-    print("\033[A")
-    print(format_attempt(language, year, day), end="  \n")
-    print(actual.rstrip())
-    if save:
-        outfile = _output_file(year, day)
-        open(outfile, "w").write(actual)
-        print(f"Saved result to {outfile}")
-
-
-def _handle_success(config, cmd, language, year, day):
-    print(
-        format_success(language, year, day),
-        end=" " if config.timing else "",
-        flush=True,
-    )
-    if not config.timing:
-        print("  ")  # Overwrite the spinner and add new line
-        return
-    try:
-        timing_info = concurrent_with_spinner(_time_with_spinner, cmd)
-        print("\033[A")
-        print(f"{format_success(language, year, day)} {timing_info}")
-    except ShellException as e:
-        print("\033[A")
-        print(format_failure(language, year, day), end="  \n")
-        print(e.stderr)
-        raise e
-    except Exception as e:
-        print("\033[A")
-        print(format_failure(language, year, day), end="  \n")
-        traceback.print_exc()
-        raise e
-
-
-def _handle_bad_output(language, year, day, expected, actual):
-    print(format_failure(language, year, day), end="  \n")
-    print(format_diff(expected, actual))
-
-
-def solve(languages, year, day, save=False):
-    outfile = _output_file(year, day)
-    expected = _find_solution(outfile)
-    if expected and save:
-        raise Exception(
-            f"Cannot save results when {outfile} alread exists, please delete it"
-        )
-    found = False
-    for l, filename in _find_files(languages, year, day):
-        found = True
-        config = language_config(l)
+        self._dispatch(SolverEvent.BUILD_START)
         try:
-            cmd = _build(config, filename, l, year, day)
-            actual = _run(cmd, l, year, day)
-            if not expected:
-                _handle_attempt(l, year, day, actual, save)
-            elif actual == expected:
-                _handle_success(config, cmd, l, year, day)
-            else:
-                _handle_bad_output(l, year, day, expected, actual)
-        except KeyboardInterrupt as e:
+            cmd = curried_build()
+            self._dispatch(SolverEvent.BUILD_END)
+            return cmd
+        except ShellException as e:
+            self._dispatch(SolverEvent.BUILD_FAILED, {"stderr": e.stderr})
             raise e
-        except:
-            continue
-    if not found and languages:
-        for l in languages:
-            print(f"{format_failure(l, year, day)} (no {l} source code found)")
+        except Exception as e:
+            self._dispatch(SolverEvent.BUILD_FAILED)
+            traceback.print_exc()
+            raise e
+
+    def _solve(self, cmd):
+        self._dispatch(SolverEvent.SOLVE_START)
+        try:
+            actual = shell_out(cmd)
+            self._dispatch(SolverEvent.SOLVE_END)
+            return actual
+        except ShellException as e:
+            self._dispatch(SolverEvent.SOLVE_ERRED, {"stderr": e.stderr})
+            raise e
+        except Exception as e:
+            self._dispatch(SolverEvent.SOLVE_ERRED)
+            traceback.print_exc()
+            raise e
+
+    def _handle_output(self, actual, outfile):
+        self._dispatch(SolverEvent.SOLVE_ATTEMPTED, {"actual": actual})
+        if outfile:
+            open(outfile, "w").write(actual)
+            self._dispatch(SolverEvent.OUTPUT_SAVED, {"file": outfile})
+
+    def _handle_success(self, cmd):
+        self._dispatch(SolverEvent.SOLVE_SUCCEEDED)
+        if not self.config.timing:
+            self._dispatch(SolverEvent.TIMING_SKIPPED)
+            return
+        self._dispatch(SolverEvent.TIMING_START)
+        try:
+            start_time = datetime.now()
+            timing_info = json.loads(shell_out(f"{cmd} --time"))
+            duration = datetime.now() - start_time
+            self._dispatch(
+                SolverEvent.TIMING_END, {"info": timing_info, "duration": duration}
+            )
+        except ShellException as e:
+            self._dispatch(SolverEvent.TIMING_FAILED, {"stderr": e.stderr})
+            raise e
+        except Exception as e:
+            self._dispatch(SolverEvent.TIMING_FAILED)
+            traceback.print_exc()
+            raise e
+
+    def _handle_invalid_output(self, expected, actual):
+        self._dispatch(
+            SolverEvent.SOLVE_FAILED, {"expected": expected, "actual": actual}
+        )
+
+
+class Solver(object):
+    def __init__(self, conn, year, day, save=False):
+        if not os.path.isdir(year):
+            raise ValueError(f"No source code exists for year {year}")
+        padded_day = day.zfill(2)
+        if not os.path.isdir(os.path.join(year, padded_day)):
+            raise ValueError(f"No source code exists for day {day} in {year}")
+        self.base_dir = os.path.join(year, padded_day)
+        self.conn = conn
+        self.year = year
+        self.day = day
+        self.save = save
+        self.outfile = os.path.join(year, day.zfill(2), "output.txt")
+        self.expected = None
+        if os.path.isfile(self.outfile):
+            self.expected = "".join(open(self.outfile, "r").readlines())
+
+    @classmethod
+    def has_solution(cls, year, day):
+        return os.path.isfile(os.path.join(year, day.zfill(2), "output.txt"))
+
+    def __call__(self, languages, save=False):
+        found = False
+        for language, filename in self._find_files(languages):
+            found = True
+            try:
+                solver = LanguageSolver(
+                    self.conn, language, self.year, self.day, filename
+                )
+                solver(self.expected, self.outfile if self.save else None)
+            except KeyboardInterrupt as e:
+                raise e
+            except:
+                continue
+        if not found and languages:
+            for language in languages:
+                self._dispatch(SolverEvent.MISSING_SRC, {"language": language})
+
+    def _dispatch(self, event, args={}):
+        args["year"] = self.year
+        args["day"] = self.day
+        self.conn.send(event)
+        self.conn.send(args)
+
+    def _find_files(self, languages):
+        if not languages:
+            languages = all_languages()
+        for language in languages:
+            ext = language_config(language).extension
+            filename = os.path.join(self.base_dir, f"main.{ext}")
+            if os.path.isfile(filename):
+                yield language, filename
---
+++ lib/terminal_display.py
@@ -0,0 +1,341 @@
+from multiprocessing import Pipe, Process
+
+from lib.languages import all_languages
+from lib.solver_event import SolverEvent
+
+
+class Color(object):
+    CYAN = "\033[96m"
+    GREEN = "\033[92m"
+    YELLOW = "\033[93m"
+    RED = "\033[91m"
+    GREY = "\033[90m"
+    ENDC = "\033[0m"
+    UNDERLINE = "\033[4m"
+
+
+class Spinner(object):
+    CHARS = ["⣷", "⣯", "⣟", "⡿", "⢿", "⣻", "⣽", "⣾"]
+    FPS = 12
+
+    def __init__(self, conn):
+        self.conn = conn
+        self.index = 0
+
+    def start(self):
+        self.index = 0
+        print(" ", end="")
+        while True:
+            print(f"\b{self.CHARS[self.index]}", end="", flush=True)
+            self.index = (self.index + 1) % len(self.CHARS)
+            if self.conn.poll(1 / self.FPS):
+                self.conn.recv()
+                break
+        print("\b", end="", flush=True)
+        self.conn.send(True)
+        self.conn.close()
+
+
+def _decimal(number):
+    parts = str(round(number, 2)).split(".")
+    return ".".join([parts[0].rjust(3, " "), parts[1].rjust(2, "0")])
+
+
+def _duration(duration):
+    if duration < 1:
+        return f"{Color.GREEN}{_decimal(duration * 1000)} ns{Color.ENDC}"
+    elif duration < 1000:
+        return f"{_decimal(duration)} μs"
+    elif duration < 1000000:
+        return f"{Color.YELLOW}{_decimal(duration / 1000)} ms{Color.ENDC}"
+    else:
+        return f"{Color.RED}{_decimal(duration / 1000000)} s{Color.ENDC}"
+
+
+def _language(language):
+    max = 0
+    for l in all_languages():
+        if len(l) > max:
+            max = len(l)
+    return language.ljust(max, " ")
+
+
+def _header(value, width):
+    if not value:
+        return " " * (width)
+    underlined = f"{Color.UNDERLINE}{value}{Color.ENDC}"
+    width += len(Color.UNDERLINE) + len(Color.ENDC)
+    lpad = " " * ((width - len(underlined)) // 2)
+    rpad = " " * (width - len(underlined) - len(lpad))
+    return f"{lpad}{underlined}{rpad}"
+
+
+def _cell(value, width, color):
+    if color:
+        colored = f"{color}{value}{Color.ENDC}"
+        width += len(color) + len(Color.ENDC)
+    else:
+        colored = value
+
+    try:
+        int(value)
+        justified = colored.rjust(width, " ")
+    except:
+        justified = colored.ljust(width, " ")
+    return f"{justified}"
+
+
+def _table(table, row_colors):
+    col_width = [0 for _ in table[0]]
+    for row in range(len(table)):
+        for col in range(len(table[row])):
+            if len(table[row][col]) > col_width[col]:
+                col_width[col] = len(table[row][col])
+    string = ""
+    for row in range(len(table)):
+        for col in range(len(table[row])):
+            if row == 0:
+                string += _header(table[row][col], col_width[col])
+            else:
+                string += _cell(table[row][col], col_width[col], row_colors[row])
+            string += "  " if col < len(table[row]) - 1 else ""
+        string += "\n"
+    return string
+
+
+def _status(language, year, day, label, color):
+    day_language = f"{year}/{day.rjust(2, '0')} {_language(language)}"
+    return f"{color}{label} [{day_language}]{Color.ENDC}"
+
+
+def format_attempt(language, year, day):
+    return _status(language, year, day, "TRY ", Color.YELLOW)
+
+
+def format_building(language, year, day):
+    return _status(language, year, day, "COMP", Color.GREY)
+
+
+def format_solving(language, year, day):
+    return _status(language, year, day, "EXEC", Color.CYAN)
+
+
+def format_success(language, year, day):
+    return _status(language, year, day, "PASS", Color.GREEN)
+
+
+def format_failure(language, year, day):
+    return _status(language, year, day, "FAIL", Color.RED)
+
+
+def format_diff(expected, actual):
+    exp_parts = expected.split("\n")
+    act_parts = actual.split("\n")
+    table = [[""], ["Expected"], ["Actual"]]
+    if len(act_parts) < 1:
+        table[0].append("Part 1")
+        table[1].append(exp_parts[0])
+        table[2].append("")
+    elif exp_parts[0] != act_parts[0]:
+        table[0].append("Part 1")
+        table[1].append(exp_parts[0])
+        table[2].append(act_parts[0])
+    if len(act_parts) < 2:
+        table[0].append("Part 2")
+        table[1].append(exp_parts[1])
+        table[2].append("")
+    elif exp_parts[1] != act_parts[1]:
+        table[0].append("Part 2")
+        table[1].append(exp_parts[1])
+        table[2].append(act_parts[1])
+    return _table(table, {1: Color.CYAN, 2: Color.YELLOW})
+
+
+def format_timing(timing_info, duration):
+    duration_us = duration.seconds * 1000000 + duration.microseconds
+    overhead = (
+        duration_us
+        - timing_info["part1"]["duration"]
+        - timing_info["part2"]["duration"]
+    )
+    part1_avg_time = (
+        timing_info["part1"]["duration"] / timing_info["part1"]["iterations"]
+    )
+    part2_avg_time = (
+        timing_info["part2"]["duration"] / timing_info["part2"]["iterations"]
+    )
+    part2_spacer = " " if part1_avg_time >= 1000000 else ""
+    overhead_spacer = " " if part2_avg_time >= 1000000 else ""
+    end_spacer = " " if overhead >= 1000000 else ""
+    contents = ", ".join(
+        [
+            f"part1: {_duration(part1_avg_time)}",
+            f"{part2_spacer}part2: {_duration(part2_avg_time)}",
+            f"{overhead_spacer}overhead: {_duration(overhead)}{end_spacer}",
+        ]
+    )
+    return f"({contents})"
+
+
+def _colorize(color, string):
+    return f"{color}{string}{Color.ENDC}"
+
+
+def _split_args(args):
+    return args["language"], args["year"], args["day"]
+
+
+def _handle_invalid_command(cmd, args):
+    print(_colorize(Color.RED, f"Invalid command {cmd} with arguments {args}"))
+
+
+def _handle_missing_src(_, args):
+    language, year, day = _split_args(args)
+    print(f"{format_failure(language, year, day)} (no source code found)")
+
+
+def _handle_build_start(display, args):
+    language, year, day = _split_args(args)
+    print(format_building(language, year, day), end=" ", flush=True)
+    display.start_spinner()
+
+
+def _handle_build_end(display, args):
+    display.stop_spinner()
+    print("\033[A")
+
+
+def _handle_build_failed(display, args):
+    display.stop_spinner()
+    language, year, day = _split_args(args)
+    print("\033[A")
+    print(format_failure(language, year, day), end="  \n")
+    if "stderr" in args:
+        print(args["stderr"], end="")
+
+
+def _handle_solve_start(display, args):
+    language, year, day = _split_args(args)
+    print(format_solving(language, year, day), end=" ", flush=True)
+    display.start_spinner()
+
+
+def _handle_solve_end(display, args):
+    display.stop_spinner()
+    print("\033[A")
+
+
+def _handle_solve_erred(display, args):
+    display.stop_spinner()
+    language, year, day = _split_args(args)
+    print("\033[A")
+    print(format_failure(language, year, day), end="  \n")
+    if "stderr" in args:
+        print(args["stderr"], end="")
+
+
+def _handle_solve_attempted(_, args):
+    language, year, day = _split_args(args)
+    print(format_attempt(language, year, day), end="  \n")
+    print(args["actual"].rstrip())
+
+
+def _handle_solve_succeeded(_, args):
+    language, year, day = _split_args(args)
+    print(
+        format_success(language, year, day), end=" ", flush=True,
+    )
+
+
+def _handle_solved_failed(_, args):
+    language, year, day = _split_args(args)
+    print(format_failure(language, year, day), end="  \n")
+    print(format_diff(args["expected"], args["actual"]))
+
+
+def _handle_output_saved(_, args):
+    print(f"Saved result to {args['file']}")
+
+
+def _handle_timing_start(display, args):
+    print(_colorize(Color.GREY, "timing"), end=" ")
+    display.start_spinner()
+
+
+def _handle_timing_skipped(_, args):
+    print("  ")  # Overwrite the spinner and add new line
+
+
+def _handle_timing_end(display, args):
+    display.stop_spinner()
+    language, year, day = _split_args(args)
+    timing_info = format_timing(args["info"], args["duration"])
+    print("\033[A")
+    print(f"{format_success(language, year, day)} {timing_info}")
+
+
+def _handle_timing_failed(display, args):
+    display.stop_spinner()
+    language, year, day = _split_args(args)
+    print("\033[A")
+    print(format_failure(language, year, day), end="  \n")
+    if "stderr" in args:
+        print(args["stderr"], end="")
+
+
+HANDLERS = {
+    SolverEvent.MISSING_SRC: _handle_missing_src,
+    SolverEvent.BUILD_START: _handle_build_start,
+    SolverEvent.BUILD_END: _handle_build_end,
+    SolverEvent.BUILD_FAILED: _handle_build_failed,
+    SolverEvent.SOLVE_START: _handle_solve_start,
+    SolverEvent.SOLVE_END: _handle_solve_end,
+    SolverEvent.SOLVE_SUCCEEDED: _handle_solve_succeeded,
+    SolverEvent.SOLVE_ERRED: _handle_solve_erred,
+    SolverEvent.SOLVE_ATTEMPTED: _handle_solve_attempted,
+    SolverEvent.SOLVE_FAILED: _handle_solved_failed,
+    SolverEvent.OUTPUT_SAVED: _handle_output_saved,
+    SolverEvent.TIMING_START: _handle_timing_start,
+    SolverEvent.TIMING_SKIPPED: _handle_timing_skipped,
+    SolverEvent.TIMING_END: _handle_timing_end,
+    SolverEvent.TIMING_FAILED: _handle_timing_failed,
+}
+
+
+class TerminalDisplay(object):
+    def __init__(self, conn):
+        self.conn = conn
+        self.spinner_conn = None
+        self.spinner_proc = None
+
+    def start_spinner(self):
+        if self.spinner_conn:
+            return
+        self.spinner_conn, conn = Pipe()
+        spinner = Spinner(conn)
+        self.spinner_proc = Process(target=spinner.start)
+        self.spinner_proc.start()
+
+    def stop_spinner(self):
+        if not self.spinner_conn:
+            return
+        self.spinner_conn.send(True)
+        self.spinner_conn.recv()
+        self.spinner_proc.join()
+        self.spinner_conn.close()
+        self.spinner_conn = None
+
+    def __call__(self):
+        running = True
+        while running:
+            if self.conn.poll(0.01):
+                cmd = self.conn.recv()
+                args = self.conn.recv()
+                if cmd in HANDLERS:
+                    HANDLERS[cmd](self, args)
+                elif cmd == SolverEvent.TERMINATE:
+                    running = False
+                    if "error" in args:
+                        print(args["error"])
+                else:
+                    _handle_invalid_command(cmd, args)
