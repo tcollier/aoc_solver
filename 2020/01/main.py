a9bad2ce094465e1150d5afa8b8b2ebf6f009f1d
Tom Collier <collier@apartmentlist.com>
2021-01-07 09:05:52 -0800

    Simplify day 1 python solution

--- 2020/01/main.py
+++ 2020/01/main.py
@@ -8,68 +8,36 @@ from lib.executor import Executor
 CWD = os.path.dirname(os.path.abspath(__file__))
 
 
-class BitVector(object):
-    NUM_BITS = 64  # Must be a power of 2
-    MASK = NUM_BITS - 1
-    SHIFTER = math.floor(math.log(NUM_BITS, 2))
-
-    def __init__(self, max_value):
-        self.bits = [0] * ((max_value >> self.SHIFTER) + 1)
-
-    def set(self, value):
-        self.bits[value >> self.SHIFTER] |= 1 << (value & self.MASK)
-
-    def test(self, value):
-        return self.bits[value >> self.SHIFTER] & 1 << (value & self.MASK) > 0
-
-    def sorted_values(self):
-        values = []
-        for i, bits in enumerate(self.bits):
-            num = i * self.NUM_BITS
-            while bits:
-                if bits & 1:
-                    values.append(num)
-                num += 1
-                bits >>= 1
-        return values
-
-
-def pair_with_sum(numbers, bit_vec, sum):
+def pair_with_sum(numbers, sum):
+    others = set(numbers)
     for num in numbers:
-        if bit_vec.test(sum - num):
+        if (sum - num) in others:
             return (num, sum - num)
     raise Exception(f"Pair with sum {sum} not found")
 
 
-def triple_with_sum(arr, sum):
-    for val_index, val in enumerate(arr):
-        i = val_index + 1
-        j = len(arr) - 1
-        while i < j:
-            if arr[i] + arr[j] + val == sum:
-                return (val, arr[i], arr[j])
-            elif arr[i] + arr[j] + val < sum:
-                i += 1
+def triple_with_sum(numbers, sum):
+    numbers.sort()
+    for i, val in enumerate(numbers):
+        j = i + 1
+        k = len(numbers) - 1
+        while j < k:
+            if val + numbers[j] + numbers[k] == sum:
+                return (val, numbers[j], numbers[k])
+            elif val + numbers[j] + numbers[k] < sum:
+                j += 1
             else:
-                j -= 1
+                k -= 1
     raise Exception(f"Triplet with sum {sum} not found")
 
 
 def part1_solution(input):
-    bit_vec = BitVector(2020)
-    for num in input:
-        bit_vec.set(num)
-    numbers = bit_vec.sorted_values()
-    pair = pair_with_sum(numbers, bit_vec, 2020)
+    pair = pair_with_sum(input, 2020)
     return pair[0] * pair[1]
 
 
 def part2_solution(input):
-    bit_vec = BitVector(2020)
-    for num in input:
-        bit_vec.set(num)
-    numbers = bit_vec.sorted_values()
-    trip = triple_with_sum(numbers, 2020)
+    trip = triple_with_sum(input, 2020)
     return trip[0] * trip[1] * trip[2]
 
 
