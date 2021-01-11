293f3d15ab732c832a2c2251aadbcf865b20868a
Tom Collier <collier@apartmentlist.com>
2021-01-10 19:09:09 -0800

    Add basic kotlin support

---
+++ lib/python/lib/lang/kotlin.py
@@ -0,0 +1,16 @@
+import os
+
+from lib.lang.registry import LanguageSettings, register_language
+
+
+@register_language(name="kotlin", extension="kt", timing=False)
+class KotlinSettings(LanguageSettings):
+    def __init__(self, file):
+        self._jar_file = file.replace(".kt", ".jar")
+        super(KotlinSettings, self).__init__(file)
+
+    def compile(self):
+        yield f"kotlinc {self.file} -include-runtime -d {self._jar_file}"
+
+    def solve(self):
+        return f"java -jar {self._jar_file}"
