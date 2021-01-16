de63dbebd323d8a46deeab1f7fc45d20f70cfdae
Tom Collier <collier@apartmentlist.com>
2021-01-15 17:10:44 -0800

    Use 4 spaces for indentation in rust

--- 2000/01/main.rs
+++ 2000/01/main.rs
@@ -3,23 +3,23 @@ use std::env;
 mod util;
 
 struct Day0Solution {
-  input: Vec<String>
+    input: Vec<String>
 }
 
 impl util::Solution for Day0Solution {
-  fn part1_result(&self) -> String {
-    self.input[0].to_string()
-  }
+    fn part1_result(&self) -> String {
+        self.input[0].to_string()
+    }
 
-  fn part2_result(&self) -> String {
-    self.input[1].to_string()
-  }
+    fn part2_result(&self) -> String {
+        self.input[1].to_string()
+    }
 }
 
 fn main() {
-  let input = vec!["Hello".to_string(), "World!".to_string()];
-  let solution = Day0Solution { input: input };
-  let executor = util::Executor::new(&solution as &util::Solution);
-  let args = env::args().collect();
-  executor.run(args);
+    let input = vec!["Hello".to_string(), "World!".to_string()];
+    let solution = Day0Solution { input: input };
+    let executor = util::Executor::new(&solution as &util::Solution);
+    let args = env::args().collect();
+    executor.run(args);
 }
